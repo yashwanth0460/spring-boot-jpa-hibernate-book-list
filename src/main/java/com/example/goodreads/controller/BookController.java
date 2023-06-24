66e91b8ba3900ccc0a5e3e40810467f9d373bcf5
yashwanth <yashwanth0861@gmail.com>
2023-06-24 17:10:50 +0000

    Book List using JPAand Hibernate

---
+++ src/main/java/com/example/goodreads/controller/BookController.java
@@ -0,0 +1,44 @@
+package com.example.goodreads.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.goodreads.model.Book;
+import com.example.goodreads.service.BookH2Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.goodreads.service.BookJpaService;
+
+import java.util.*;
+
+@RestController
+class BookController {
+    @Autowired
+    public BookJpaService bookService;
+
+    @GetMapping("/books")
+    public ArrayList<Book> getBooks() {
+        return bookService.getBooks();
+    }
+
+    @GetMapping("/books/{bookId}")
+    public Book getBookById(@PathVariable("bookId") int bookId) {
+        return bookService.getBookById(bookId);
+    }
+
+    @PostMapping("/books")
+    public Book addBook(@RequestBody Book book) {
+        return bookService.addBook(book);
+
+    }
+    
+    @PutMapping("/books/{bookId}")
+    public Book updateBook(@PathVariable("bookId") int bookId, @RequestBody Book book) {
+        return bookService.updateBook(bookId, book);
+    }
+    
+    @DeleteMapping("/books/{bookId}")
+    public void deleteBook(@PathVariable("bookId") int bookId) {
+        bookService.deleteBook(bookId);
+    }
+
+
+}
